5a1c5a09c0cb64c0b5c6ec1d37beb67f53569df7
Kamya Domnnick <domisemak@gmail.com>
2021-11-01 12:54:42 +0300

    feat(MostRead): add fetch data functionality from  most read

---
+++ nytimesarticles/src/components/Results.js
@@ -0,0 +1,43 @@
+import React, {useEffect, useState}from 'react';
+import './css/Styles.css';
+
+const Results= ()=>{
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=wOvranaebS0fZYSYy6oPnPLhIKk4kALO')
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            setData(data.results);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      }, []);
+    
+      if (loading) {
+        return <p>Data is loading...</p>;
+      }
+    
+      return (
+        <div className='card'>
+         <h2>NY Times Most Read</h2>
+          {data.map((item) => (
+            <div className='container'>
+            <div key={item.id} style={{margin:'1rem'}}>{item.title}</div>
+            <div key={item.id} style={{display:'inline-block',marginLeft:40, color:'grey'}}>{item.byline}</div>
+            <div key={item.id} style={{display:'inline-block',marginLeft:40, color:'grey'}}>{item.published_date}</div>
+            </div>
+          ))}
+        </div>
+      );
+    
+};
+
+export default Results;
